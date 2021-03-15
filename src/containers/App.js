1d6c2867ab5cef43b84dc99d49a363c5b9049753
Irena Jurasek <jurasek@eurisnet.it>
2021-03-15 12:40:35 +0100

    add Router to App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
-import Header from '../components/Header/Header';
-import Feed from './Feed';
+import React, { Component } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import Header from "../components/Header/Header";
+import Feed from "./Feed";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -26,6 +27,9 @@ class App extends Component {
         <GlobalStyle />
         <AppWrapper>
           <Header />
+          <Router>
+            <Route exact path="/" component={Feed} />
+          </Router>
           <Feed />
         </AppWrapper>
       </>
